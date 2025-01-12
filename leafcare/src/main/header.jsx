e84516d690255cca8107fbd3b27c5c4226fc4a6f
subham-chaudhary <shubhu.uwu@gmail.com>
2025-01-12 08:49:24 +0530

    added: logo, offcancavs sidebar

--- leafcare/src/main/header.jsx
+++ leafcare/src/main/header.jsx
@@ -1,14 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import OffcanvasNavbar from './sidebar';
+import logo from '../assets/logo-leafcare-removebg.png'
 import './header.css';
 function header() {
     return (
         <>
             {['lg'].map((expand) => (
-                <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
-                    <Container fluid>
-                        <Navbar.Brand href="#">LeafCare</Navbar.Brand>
+                <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3" fixed='top' >
+                    <Container fluid><Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src = {logo}
+                            width="40"
+                            height="40"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        LeafCare
+                    </Navbar.Brand>
                         <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
                         <OffcanvasNavbar />
                     </Container>
